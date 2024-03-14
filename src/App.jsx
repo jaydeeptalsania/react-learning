3faab5885419c18a2ee1404b29e8885057d81e65
Admin <jaydeept007@gmail.com>
2024-03-14 16:46:44 +0530

    created dynamic component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import TestComponent from "./TestComponent";
+import DynamicComponent from "./DynamicComponent";
 
 function App(){
   return <div>
     <h1>Hello there</h1>
     <TestComponent/>
+    <DynamicComponent/>
   </div>
   
 }
---
+++ src/DynamicComponent.jsx
@@ -0,0 +1,7 @@
+export default function DynamicComponent (){
+   let firstName = 'Jaydeep';
+
+    return <h3>
+        Hello :- {firstName}
+    </h3>
+}
