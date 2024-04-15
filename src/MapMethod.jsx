d6bc249dbf766bbea1252b33d55c299e899e8afa
Admin <jaydeept007@gmail.com>
2024-04-15 17:42:32 +0530

    Conditional redering

--- src/MapMethod.jsx
+++ src/MapMethod.jsx
@@ -2,6 +2,7 @@ function MapMethod(){    // instead of 'for loop' use 'map method' to iterate el
  let myItems = ['mouse','keyboard','printer','charger'];
   return(
     <>
+     {myItems.length === 0 ? <h3>The list is empty</h3>:null}
      {myItems.map((item ,index) => <p key={index}>{item}</p>)}
     </>
   )
